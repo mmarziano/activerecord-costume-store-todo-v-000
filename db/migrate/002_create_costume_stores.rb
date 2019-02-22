44a8b10227b4b09ab6ee4d94f1d399eea6dbe8be
Monica Marziano <mmarziano1@gmail.com>
2019-02-22 10:24:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,7 @@
 
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
    def change 
-    create_table :costumes_stores do |t|
+    create_table :costume_stores do |t|
       t.string :name 
       t.integer :costume_inventory 
       t.integer :number_of_employees 
