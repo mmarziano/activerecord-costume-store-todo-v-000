165e6a38327d6582fd036cd24fbec0c8ae186099
Monica Marziano <mmarziano1@gmail.com>
2019-02-22 10:28:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location
       t.string :theme 
       t.integer :price 
-      t.string :family_friendly?
+      t.string :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :long_description
